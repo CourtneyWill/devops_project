751adac27d82f80881efeedf1ae3a3e25bd857f9
williamsc51 <williams.a.courtney@gmail.com>
2019-06-11 09:09:40 -0400

    update input to prompt for act number or lat/lng

--- init.rb
+++ init.rb
@@ -6,10 +6,25 @@ puts '-' * 15
 puts '| Open 311 API |'
 puts '-' * 15
 
-puts 'To get a list of request please enter an account number'
+puts 'Press 1. To get a list of request by account number'
+puts 'Press 2. To get a list of request by lat/lng coordinates'
 print '> '
 
-account = UserInput.new
-account.actnum = gets.chomp
+input = gets.chomp
 
-UserInput.getRequestByAccount(account.actnum)
+account = UserInput.new
+
+case input.to_i
+    when 1
+        puts 'Enter the account number account number: '
+        account.actnum = gets.chomp
+        UserInput.getRequestByAccount(account.actnum)
+    when 2
+        puts 'Enter the lattitude coordinates: '
+        account.lat = gets.chomp
+        puts 'Enter the longitude coordinates: '
+        account.lng = gets.chomp
+        UserInput.getRequestByLatLng(account.lat, account.lng)
+end
+
+# 
